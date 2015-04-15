65a440ec53ee972e6f0bc1683c3301c108a4b4e6
anderson <anderson@foscarini.biz>
2015-04-15 15:30:32 -0300

    dbinit

--- code/xmlReader.py
+++ code/xmlReader.py
@@ -1,6 +1,16 @@
 __author__ = 'anderson'
 
 import xml.etree.ElementTree as ET
+import MySQLdb
 
-tree = ET.parse('country_data.xml')
+tree = ET.parse('teste.xml')
 root = tree.getroot()
+
+
+
+con = MySQLdb.connect(host='localhost', user='root', passwd='showtime',db='teste')
+c = con.cursor()
+for i in xrange(5):
+    c.execute("INSERT INTO t1 VALUES ('numero%s', %i)"%(i, i))
+
+con.commit()
