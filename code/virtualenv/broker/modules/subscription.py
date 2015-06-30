b3ff22d8affdd3535102a33d910e817168adc86b
Anderson Foscarini <anderson@foscarini.biz>
2015-06-30 15:13:22 -0300

    thesis

--- code/virtualenv/broker/modules/subscription.py
+++ code/virtualenv/broker/modules/subscription.py
@@ -24,21 +24,26 @@ def subscribe(callback_url, entity_name, entity_type, scope_list, minutes):
         ######################MONGODB
         client = MongoClient()
         db = client.broker
-        scopes_ids = []
-        for result in db.scopes.find({'name': {'$in': scope_list.split(',')}}, {'_id': 1}):
-            scopes_ids.append(result["_id"])
-        entity_el = db.entities.find_one({'name': entity_name, 'type': entity_type}, {'_id': 1})
-        if entity_el and len(scopes_ids) > 0:
+        scopes = []
+        for result in db.scopes.find({'name': {'$in': scope_list.split(',')}}):
+            scopes.append(result)
+        entity_el = db.entities.find_one({'name': entity_name, 'type': entity_type})
+        if entity_el and len(scopes) > 0:
             db.subscriptions.insert_one({'callback_url': callback_url, 'minutes': minutes,
-                                         'entity_id': entity_el["_id"], 'scopes': scopes_ids})
+                                         'entity': entity_el, 'scopes': scopes})
             return generic_response.generate_response('OK','200','Subscription Success',
                                                       'subscription','','',entity_name,entity_type,scope_list)
         else:
-            logger.warn('subscription - Bad Parameters - callbackUrl: %s; entity: %s; scope_list: %s; minutes: %s')
+            logger.warn('subscription - Bad Parameters - callbackUrl: %s; entity: %s; scope_list: %s; minutes: %s',
+                        callback_url, entity_type+'|'+entity_name, scope_list, minutes)
             return generic_response.generate_response('ERROR','400','Bad Parameters',
                                                       'subscription','','',entity_name,entity_type,scope_list)
         #####################MONGODB
 
+    except pymongo.errors.DuplicateKeyError:
+        logger.warn('subscription - DuplicateKeyError')
+        return generic_response.generate_response('ERROR','400','Duplicate Subscription',
+                                                  'subscription','','','','','')
     except Exception as e:
         logger.warn('subscription - Internal Error: %s', sys.exc_info()[0])
         error_message = "Internal Error"
